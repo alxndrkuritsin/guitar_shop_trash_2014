c09bca0ca37cc55514e18f5b137100446e988668
alxndrkuritsin <alxndrkuritsin@gmail.com>
2024-02-02 11:18:45 +0300

    Update order.php
    formatting

--- html/views/pages/order.php
+++ html/views/pages/order.php
@@ -1,37 +1,37 @@
 <h2 align="center">Оформление заказа</h2>
 <form action="index.php?view=upload" method="post" id="cart-form">
 <table id="mycart" align="center" cellspacing="0" cellpadding="0" border="1" width="550" bgcolor = "white">
-<tr>
-    <th>Товар</th>
-    <th>Цена</th>
-    <th>Кол-во</th>
-    <th>Всего</th>
-</tr>
+	<tr>
+    	    <th>Товар</th>
+	    <th>Цена</th>
+	    <th>Кол-во</th>
+	    <th>Всего</th>
+	</tr>
 
 <?php 
-foreach($_SESSION['cart'] as $id => $quantity):
-$product = get_product($id);
+	foreach($_SESSION['cart'] as $id => $quantity):
+	$product = get_product($id);
 ?>
 
 <tr>
-    <td align="center"><?php echo $product['title']; ?></td>
-    <td align="center"><?php echo number_format($product['price'],2);  ?>руб.</td>
-    <td align="center"><?php echo $quantity ?></td>
-    <td align="center"><?php echo number_format($product['price'] * $quantity,2); ?>руб.</td>  
+    <td align="center"><?php echo $product['title'];?></td>
+    <td align="center"><?php echo number_format($product['price'],2);?>руб.</td>
+    <td align="center"><?php echo $quantity?></td>
+    <td align="center"><?php echo number_format($product['price'] * $quantity,2);?>руб.</td>  
 </tr>
-<?php endforeach; ?>
+<?php endforeach;?>
 </table>	
 	<p align="center">
-	Ваше имя: <br />
-	<input type="text" name="name"/><br />
-	Ваша фамилия: <br />
-	<input type="text" name="s_name"/><br />
-	Ваша адрес: <br />
-	<input type="text" name="address"/><br />
-	Почтовый индекс: <br />
-	<input type="text" name="post_index"/><br />
-	Ваш e-mail: <br />
-	<input type="text" name="email"/><br />
+		Ваше имя: <br/>
+		<input type="text" name="name"/><br/>
+		Ваша фамилия: <br/>
+		<input type="text" name="s_name"/><br />
+		Ваша адрес: <br/>
+		<input type="text" name="address"/><br />
+		Почтовый индекс: <br/>
+		<input type="text" name="post_index"/><br />
+		Ваш e-mail: <br/>
+		<input type="text" name="email"/><br />
 	</p>
 	
     <p align="center"><input type="submit" name="order" value=" Заказать " /></p>
