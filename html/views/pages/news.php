3d8045b70e2054dfb9d418d235a815bcfee81e5a
alxndrkuritsin <alxndrkuritsin@gmail.com>
2024-02-02 11:19:59 +0300

    Update news.php
    formatting

--- html/views/pages/news.php
+++ html/views/pages/news.php
@@ -1,10 +1,10 @@
 ﻿<?php
-$news = get_news();
-foreach($news as $item):?>
-<br/><font size="3" face="Arial"><?php echo $item['date'] ?></font>
-<table border="1" bgcolor="white">
-<tr>
-<td><p><font size="4"><?php echo $item['news'] ?></font></td>
-</tr>
-</table>
+  $news = get_news();
+  foreach($news as $item):?>
+    <br/><font size="3" face="Arial"><?php echo $item['date'] ?></font>
+    <table border="1" bgcolor="white">
+      <tr>
+        <td><p><font size="4"><?php echo $item['news'] ?></font></td>
+      </tr>
+    </table>
 <?php endforeach ?>
